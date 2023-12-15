4caa744f610ad1043aacde74868ba266cc605ee8
IslamProg <74923378+IslamLessom@users.noreply.github.com>
2023-12-15 18:31:59 +0300

    client

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,10 +1,26 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import { store } from './app/store';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 import './index.css';
+import { Paths } from './paths';
+
+const router = createBrowserRouter([
+  {
+    path: Paths.login,
+    element: <h1>Login</h1>
+  },
+  {
+    path: Paths.register,
+    element: <h1>Register</h1>
+  },
+  {
+    path: Paths.home,
+    element: <h1>home</h1>
+  },
+])
 
 const container = document.getElementById('root')!;
 const root = createRoot(container);
@@ -12,7 +28,7 @@ const root = createRoot(container);
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <RouterProvider router={router} />
     </Provider>
   </React.StrictMode>
 );
