b4b4e200d404c3d4ae34bfbddcf111373e7d5a71
IslamProg <74923378+IslamLessom@users.noreply.github.com>
2023-12-30 17:06:32 +0300

    Add files via upload

--- client/src/components/employee-form/index.tsx
+++ client/src/components/employee-form/index.tsx
@@ -1,21 +1,32 @@
-import { Employee } from "@prisma/client"
-import { Card } from "antd"
+import { Employee } from "@prisma/client";
+import { Card, Form, Space } from "antd";
+import { CustomInput } from "../custom-input";
+import { ErrorMessage } from "../error-message";
+import { CustomButton } from "../custom-button";
 
 type Props<T> = {
-  onFinish: (value: T) => void
-  btnText: string
-  title: string
-  error?: string
-  employee?: string
-}
+  onFinish: (value: T) => void;
+  btnText: string;
+  title: string;
+  error?: string;
+  employee?: string;
+};
 
-export const EmployeeForm = ({
-  onFinish,
-  title,
-}:Props<Employee>) => {
+export const EmployeeForm = ({ onFinish, title }: Props<Employee>) => {
   return (
-    <Card title={title} style={{width: '30rem'}}>
-
+    <Card title={title} style={{ width: "30rem" }}>
+      <Form name="employee-form" onFinish={onFinish} initialValues={employee}>
+        <CustomInput type="text" name="firstName" placeholder="Имя" />
+        <CustomInput type="text" name="lastName" placeholder="Фамилия" />
+        <CustomInput type="number" name="age" placeholder="Возраст" />
+        <CustomInput type="text" name="address" placeholder="Адресс" />
+        <Space>
+          <ErrorMessage message={error} />
+          <CustomButton htmlType="submit">
+            {btnText}
+          </CustomButton>
+        </Space>
+      </Form>
     </Card>
-  )
-}
+  );
+};
--- client/src/pages/add-employee/index.tsx
+++ client/src/pages/add-employee/index.tsx
@@ -1,11 +1,28 @@
+import React, {useState} from 'react'
+import { EmployeeForm } from '../../components/employee-form'
 import { Layout } from '../../components/layout'
 import { Row } from 'antd'
+import { useNavigate } from 'react-router-dom'
+import { useSelector } from 'react-redux'
+import { useAddEmployeesMutation } from '../../app/services/employess'
 
 export const AddEmployee = () => {
+  const [error, setError] = useState()
+  const navigate = useNavigate()
+  const user = useSelector()
+  const [addEmployee] = useAddEmployeesMutation()
+  const handleAddEmployee = () => {
+
+  }
   return (
     <Layout>
       <Row align='middle' justify='center'>
-
+        <EmployeeForm 
+          title="Добавить сотрудника"
+          btnText="Добавить"
+          onFinish={handleAddEmployee}
+          error={error}
+        />
       </Row>
     </Layout>
   )
